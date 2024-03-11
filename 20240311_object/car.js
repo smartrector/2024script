8f0ad63cfa0e1707f6c7ac1959e82c5c703776b3
smartrector <smartrector@gmail.com>
2024-03-11 17:13:25 +0900

    upgit

---
+++ 20240311_object/car.js
@@ -0,0 +1,49 @@
+var car = [
+  {
+    name: "sonata",
+    speed: 10,
+    color: "white",
+    door: 4,
+    start: function () {
+      return this.speed;
+    },
+  },
+  {
+    name: "carnival",
+    speed: 10,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+  {
+    name: "carnival1",
+    speed: 10,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+  {
+    name: "carnival2",
+    speed: 10,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+  {
+    name: "carnival3",
+    speed: 10,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+];
+
+export default car;
