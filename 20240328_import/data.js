9364a03b44c00a02de36f59bddb98bf588c58c6b
smartrector <smartrector@gmail.com>
2024-03-28 11:34:11 +0900

    import

---
+++ 20240328_import/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    idx: 0,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "2024.03.06",
+    img: "testimg.jpg",
+  },
+  {
+    idx: 2,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "2024.03.06",
+    img: "testimg.jpg",
+  },
+  {
+    idx: 3,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "2024.03.06",
+    img: "testimg.jpg",
+  },
+  {
+    idx: 4,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "2024.03.06",
+    img: "testimg.jpg",
+  },
+];
+
+export default data;
